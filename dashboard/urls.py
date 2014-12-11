4b18bbc8ac29537b28d91193f89a03fde404d34d
Paul Grant <paul.grant@vertu.com>
2014-12-11 14:33:58 +0000

    Working inventory home page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,18 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'inventory.views.home'),
-    url(r'^away$', 'main.views.away')
-))
+    url(r'^$', 'inventory.views.home', name='inventory_home'),
+    url(r'^away$', 'inventory.views.away')
+)
+
+urlpatterns += patterns(
+    'django.contrib.auth.views',
+
+    url(r'^login/$', 'login',
+        {'template_name': 'login.html'},
+        name='dashboard_login'),
+
+    url(r'^logout/$', 'logout',
+        {'next_page': 'boardgames_home'},
+        name='dashboard_logout'),
+)
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, "inventory/home.html")
+
+
+def away(request):
+    return HttpResponse("Hello, Away World!")
